8ee003fb68becb3da2b46866417927a2d4bb05f8
Antonio Lagrotteria <lagrotteri@gmail.com>
2015-10-19 00:17:19 +0200

    Initial Commit

---
+++ public/client.js
@@ -0,0 +1,134 @@
+$( document ).ready(function() {
+
+    var formMap = new Map();
+    formMap.set(".api-url-form", getAPIUrl);
+    formMap.set(".client-secret-form", getLoginKey);
+    formMap.set(".report-suite-form", getReportSuites);
+    formMap.set(".variables-form", getVariables);
+
+    var API = "http://127.0.0.1:3002/api";
+
+    $(".demo-section").hide();
+    $(".api-url-result").hide();
+    $(".client-secret-result").hide();
+    $(".report-suite-result").hide();
+    $(".variables-result").hide();
+    $(".report-suite-section").hide();
+    $(".variables-section").hide();
+    $(".other-section").hide();
+
+    Ladda.bind( 'button[type=submit]' );
+
+    /**
+     * We use foo/bar user to get the generic api
+     */
+    function getAPIUrl() {
+
+        return $.ajax({
+            url: API + "/companies?method=Company.GetEndpoint",
+            method: "POST",
+            data: {
+                apiURL: window.api_url,
+                company: $("#company-field").val(),
+                login: $("#login-field").val() || "foo",
+                password: $("#password-field").val() || "bar"
+            }
+        })
+        .done(function( data ) {
+            window.api_url = data;
+            $(".api-url-value").text(window.api_url);
+            $(".api-url-result").show();
+            $(".demo-section").show();
+        });
+    }
+
+    function getLoginKey() {
+
+        var company = $("#company-field").val(),
+            login = $("#login-field").val(),
+            password = $("#password-field").val();
+
+        return $.ajax({
+            url: API + "/companies?method=Company.GetLoginKey",
+            method: "POST",
+            data: {
+                apiURL: window.api_url,
+                company: company,
+                login : login,
+                password : password
+            }
+        })
+        .done(function( data ) {
+            window.username = login + ":" + company;
+            window.client_secret = data;
+            $(".client-secret-value").text(data);
+            $(".client-secret-result").show();
+            $(".report-suite-section").show();
+        });
+    }
+
+    function getReportSuites () {
+        return $.ajax({
+            url: API + "/reports?method=Company.GetReportSuites",
+            method: "POST",
+            data: {
+                apiURL: window.api_url,
+                sp: $("#rsid-field").val(),
+                username: window.username,
+                secret: window.client_secret
+            }
+        })
+        .done(function( data ) {
+            var rs = data.result.report_suites[0],
+                rsid = rs.rsid,
+                rsName = rs.site_title;
+            $(".company-get-report-suites-result").append(rsid + " " + rsName);
+            $(".report-suite-result").show();
+            $(".variables-section").show();
+        });
+    }
+
+    function getVariables() {
+        return $.ajax({
+            url: API + "/variables?method=Report.GetElements",
+            data: {
+                reportSuiteID: $("#rsid-field").val(),
+                username: window.username,
+                secret: window.client_secret
+            }
+        })
+        .done(function( data ) {
+            data.result.forEach(function(report, index) {
+                $(".report-get-elements-result").append((index+1) + ") " + report.id + " - " + report.name + "<br>");
+            })
+
+            $(".variables-result").show();
+            $(".other-section").show();
+        });
+    }
+
+    function handleSubmission ($button, callback) {
+        var l = Ladda.create($button[0]);
+        l.start();
+
+        callback()
+            .always(function() { l.stop(); });
+
+        return false;
+    }
+
+    function setupSubmission (formClass) {
+        var $form = $(formClass);
+
+        $form.submit(function (e) {
+                return handleSubmission($form.find("button"), formMap.get(formClass));
+            });
+    }
+
+    setupSubmission(".api-url-form");
+    setupSubmission(".client-secret-form");
+    setupSubmission(".report-suite-form");
+    setupSubmission(".variables-form");
+});
+
+
---
+++ server.js
@@ -0,0 +1,131 @@
+var express = require('express'),
+    request = require('request'),
+    wsse = require('./public/js/wsse.js');
+
+
+// Variables
+var DEFAULT_API_URL = "https://api.omniture.com/admin/1.4/rest/";
+
+var app = express();
+app.use('/', express.static(__dirname + '/public'));
+
+var bodyParser = require('body-parser')
+app.use( bodyParser.json() );       // to support JSON-encoded bodies
+app.use(bodyParser.urlencoded({     // to support URL-encoded bodies
+    extended: true
+}));
+
+app.post('/api/companies', function(req, res) {
+
+    var method = req.query.method,
+        apiURL = req.body.apiURL || DEFAULT_API_URL,
+        company = req.body.company,
+        login = req.body.login,
+        password = req.body.password,
+        username = login + ":" + company
+
+    var options = {
+        url: apiURL,
+        qs: {
+            method: method
+        },
+        json: {
+            company: company,
+            login : login,
+            password : password
+        },
+        headers: {
+            "X-WSSE": getToken(username, password)
+        },
+        method: "POST"
+    };
+
+
+    request(options, function (error, response, body) {
+        res.send(body);
+    });
+});
+
+
+app.post('/api/reports', function(req, res) {
+
+    var method = req.query.method,
+        apiURL = req.body.apiURL,
+        searchQuery = req.body.sp,
+        username = req.body.username,
+        secret = req.body.secret;
+
+    var options = {
+        url: apiURL,
+        qs: {
+            method: method
+        },
+        json: {
+            sp: searchQuery
+        },
+        headers: {
+            "X-WSSE": getToken(username, secret)
+
+        },
+        method: "POST"
+    };
+
+
+    request(options, function (error, response, body) {
+        res.send({result : body})
+    });
+});
+
+
+app.post('/api/variables', function(req, res) {
+
+    var method = req.query.method,
+        apiURL = req.body.apiURL,
+        rsid = req.query.reportSuiteID,
+        username = req.body.username,
+        secret = req.body.secret;
+
+    var options = {
+        url: apiURL,
+        qs: {
+            method: method
+        },
+        json: {
+            reportSuiteID: rsid
+        },
+        headers: {
+            "X-WSSE": getToken(username, secret)
+
+        },
+        method: "POST"
+    };
+
+
+    request(options, function (error, response, body) {
+        var interestingVariables = [];
+        body.filter(function(report) {
+            if(report.id.indexOf("evar") !== -1 ||
+                report.id.indexOf("prop")!== -1 ||
+                report.id.indexOf("event")!== -1) {
+                interestingVariables.push(report);
+            }
+        })
+
+        res.send({result : interestingVariables})
+    });
+});
+
+
+function getToken(username, secret) {
+    return wsse.wsseHeader(username, secret);
+}
+
+var server = app.listen(3002, function () {
+
+    var host = server.address().address
+    var port = server.address().port
+
+    console.log("Example app listening at http://%s:%s", host, port)
+})
+
+
